08a364f86e858e0b18894c9c34c8037b741475e3
nike <maxsallis@outlook.com>
2024-02-18 21:50:03 +0500

    Added commit 1

---
+++ tapigo/components/Form.vue
@@ -0,0 +1,75 @@
+<template>
+    <div class="flex items-center justify-content h-screen">
+      <form @submit.prevent="submitForm" class="max-w-md mx-auto">
+        <div class="mb-4">
+          <label for="fullname" class="block text-gray-700 text-sm font-bold mb-2">Имя Фамилия</label>
+          <input v-model="fullname" type="text" id="fullname" name="fullname" :class="{ 'border-red-500': fullnameError }" class="transition duration-500 bg-gray-100 hover:bg-gray-200 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
+          <p v-if="fullnameError" class="text-red-500 text-xs italic">Пожалуйста, введите кириллицей имя и фамилию.</p>
+        </div>
+        <div class="mb-4">
+          <label for="phone" class="block text-gray-700 text-sm font-bold mb-2">Номер телефона</label>
+          <input v-model="phone" type="text" id="phone" name="phone" :class="{ 'border-red-500': phoneError }" class="transition duration-500 bg-gray-100 hover:bg-gray-200 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
+          <p v-if="phoneError" class="text-red-500 text-xs italic">Пожалуйста, введите номер начинающийся с +7.</p>
+        </div>
+        <div class="mb-4">
+          <label for="email" class="block text-gray-700 text-sm font-bold mb-2">Email</label>
+          <input v-model="email" type="email" id="email" name="email" class="transition duration-500 bg-gray-100 hover:bg-gray-200 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
+         </div>
+        <div class="mb-4">
+          <label for="password" class="block text-gray-700 text-sm font-bold mb-2">Пароль</label>
+          <input v-model="password" type="password" id="password" name="password" class="transition duration-500 bg-gray-100 hover:bg-gray-200 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
+        </div>
+        <div class="mb-4">
+          <button type="submit" class="transition duration-500 bg-red-500 hover:bg-red-900 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Отправить</button>
+        </div>
+      </form>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        fullname: '',
+        phone: '',
+        email: '',
+        password: '',
+        fullnameError: false,
+        phoneError: false
+      };
+    },
+    methods: {
+      submitForm() {
+        // Проверка валидности ФИО
+        const fullnamePattern = /^[а-яА-ЯёЁ\s]+$/;
+        if (!fullnamePattern.test(this.fullname)) {
+          this.fullnameError = true;
+          return;
+        } else {
+          this.fullnameError = false;
+        }
+  
+        // Проверка валидности номера телефона
+        const phonePattern = /^\+?\d{10,14}$/;
+        if (!phonePattern.test(this.phone)) {
+          this.phoneError = true;
+          return;
+        } else {
+          this.phoneError = false;
+        }
+  
+        // Отправка данных формы (здесь можно добавить дополнительную логику отправки)
+        console.log('Отправка данных:', {
+          fullname: this.fullname,
+          phone: this.phone,
+          email: this.email,
+          password: this.password
+        });
+      }
+    }
+  };
+  </script>
+  
+  <style>
+  /* Здесь можно добавить стили, если требуется */
+  </style>
---
+++ tapigo/components/maketFigma.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="top bg-gradient-to-b from-gray-900 to-gray-800 py-16 px-8 lg:px-60">
+    <div class="container mx-auto">
+      <!-- Первый блок -->
+      <div class="flex flex-col-reverse lg:flex-row jusify-between space-y-8 lg:space-y-0 lg:space-x-8 lg:items-center">
+        <!-- Левая часть -->
+        <div class="left flex flex-col justify-center lg:w-1/2 py-8 px-4 md:px-8 lg:px-16 lg:mr-auto mg-8 lg:mb-0">
+          <h2 class="text-3xl font-bold text-white mb-4">We do campaigns that works well for your idea</h2>
+          <p class="text-white text-lg mb-4">Our documentary campaigns feature leading figures, organisations and leaders, in open and candid discussions about transformative strategies and mindsets. Addressing complex trends.</p>
+          <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Кнопка</button>
+        </div>
+        <!-- Правая часть -->
+        <div class="right lg:w-1/2 lg:ml-auto mb-8 lg:mb-0">
+          <img src="right.jpg" alt="Изображение" class="w-full rounded-3xl">
+        </div>
+      </div>
+      <!-- Второй блок -->
+      <div class="flex flex-col lg:flex-row mt-16">
+        <!-- Левая часть -->
+        <div class="left lg:w-1/2">
+          <img src="left.jpg" alt="Изображение" class="w-full rounded-3xl">
+        </div>
+        <!-- Правая часть -->
+        <div class="right flex flex-col justify-center lg:w-1/2 py-8 px-4 md:px-8 lg:px-16">
+          <h2 class="text-3xl font-bold text-white mb-4">We do campaigns that works well for your idea</h2>
+          <p class="text-white text-lg mb-4">Our documentary campaigns feature leading figures, organisations and leaders, in open and candid discussions about transformative strategies and mindsets. Addressing complex trends.</p>
+          <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Кнопка</button>
+        </div>
+      </div>
+    </div>
+</div>
+  </template>
+  
+  <script>
+  export default {
+    // Добавьте вашу логику Vue здесь, если необходимо
+  }
+  </script>
+  
+  <style scoped>
+  /* Добавьте ваши стили CSS здесь, если необходимо */
+  </style>
