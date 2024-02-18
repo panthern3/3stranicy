9e4928b7897750205301d32e6ddd1344978010b0
nike <maxsallis@outlook.com>
2024-02-18 23:26:19 +0500

    Added Adaptive

--- tapigo/components/maketFigma.vue
+++ tapigo/components/maketFigma.vue
@@ -1,13 +1,14 @@
 <template>
     <div class="top bg-gradient-to-b from-gray-900 to-gray-800 py-16 px-8 lg:px-60">
-    <div class="container mx-auto">
+    <div class="container mx-auto my-auto">
       <!-- Первый блок -->
-      <div class="flex flex-col-reverse lg:flex-row jusify-between space-y-8 lg:space-y-0 lg:space-x-8 lg:items-center">
+      <div class="flex flex-col-reverse lg:flex-row jusify-between space-y-8 lg:space-y-0 lg:space-x-8 lg:items-center lg:relative right-8">
         <!-- Левая часть -->
         <div class="left flex flex-col justify-center lg:w-1/2 py-8 px-4 md:px-8 lg:px-16 lg:mr-auto mg-8 lg:mb-0">
           <h2 class="text-3xl font-bold text-white mb-4">We do campaigns that works well for your idea</h2>
           <p class="text-white text-lg mb-4">Our documentary campaigns feature leading figures, organisations and leaders, in open and candid discussions about transformative strategies and mindsets. Addressing complex trends.</p>
-          <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Кнопка</button>
+          <button class="text-left w-40 text-transparent bg-gradient-to-r from-purple-600 to-indigo-600 bg-clip-text font-bold rounded">All campaigns</button>
+          <img src="arrow.svg" class="w-3 h-3 relative left-28 bottom-4" alt="arrow">
         </div>
         <!-- Правая часть -->
         <div class="right lg:w-1/2 lg:ml-auto mb-8 lg:mb-0">
@@ -15,7 +16,7 @@
         </div>
       </div>
       <!-- Второй блок -->
-      <div class="flex flex-col lg:flex-row mt-16">
+      <div class="flex flex-col lg:flex-row mt-16 lg:relative left-8">
         <!-- Левая часть -->
         <div class="left lg:w-1/2">
           <img src="left.jpg" alt="Изображение" class="w-full rounded-3xl">
@@ -23,8 +24,9 @@
         <!-- Правая часть -->
         <div class="right flex flex-col justify-center lg:w-1/2 py-8 px-4 md:px-8 lg:px-16">
           <h2 class="text-3xl font-bold text-white mb-4">We do campaigns that works well for your idea</h2>
-          <p class="text-white text-lg mb-4">Our documentary campaigns feature leading figures, organisations and leaders, in open and candid discussions about transformative strategies and mindsets. Addressing complex trends.</p>
-          <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Кнопка</button>
+          <p class="text-white text-lg mb-4">This exclusive service provided by our team of professional content creators is available for global use. Our clients will have access and total creative control throughout the process of production, with resources and talent supplied by us.</p>
+          <button class="text-left w-44 text-transparent bg-gradient-to-r from-purple-600 to-indigo-600 bg-clip-text font-bold rounded">I have an idea for film</button>
+          <img src="arrow.svg" class="w-3 h-3 relative left-44 bottom-4" alt="arrow">
         </div>
       </div>
     </div>
